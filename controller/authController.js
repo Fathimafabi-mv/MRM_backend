cd6892dd9c653e4f56abbe62eac81f656d58b9c4
Nihal <nihal.pm9633@gmail.com>
2025-03-05 19:57:20 +0530

    user edit created

--- controller/authController.js
+++ controller/authController.js
@@ -29,7 +29,7 @@ router.post("/register",catchAsyncError( async(req,res,next)=>{
 
 }))
 
-router.post("/login",catchAsyncError(async(req,res)=>{
+router.post("/login",catchAsyncError(async(req,res,next)=>{
     try {
         const{username,password}=req.body;
         if(!username || !password){
@@ -50,6 +50,47 @@ router.post("/login",catchAsyncError(async(req,res)=>{
     }
    
 
+}))
+router.get("/getusers",catchAsyncError(async(req,res, next)=>{
+    try {
+        const allUser = await userModel.find()
+        return res.status(200).json(
+            {allUser}
+        )
+    } catch (error) {
+        return next(new ErrorHandler(error.message,400))
+    }
+}))
+
+router.delete("/deleteuser/:id", catchAsyncError(async(req,res,next)=>{
+    try {
+        const {id} = req.params
+        const user = await userModel.findByIdAndDelete(id)
+        return res.status(200).json({message: "user dealeted", user})
+    } catch (error) {
+        return next(new ErrorHandler(error.message,400))
+    }
+}))
+
+router.patch('/updateuser/:id', catchAsyncError(async(req,res,next)=>{
+    try {
+        const {id} = req.params
+        const {username,shopname,address,phonenumber,whatsappno,TRNno,location} = req.body
+
+        const updateUser = {}
+        if(username) updateUser.username = username
+        if(shopname) updateUser.shopname = shopname
+        if(address) updateUser.address = address
+        if(location) updateUser.location = location
+        if(phonenumber) updateUser.phonenumber = phonenumber
+        if(whatsappno) updateUser.whatsappno = whatsappno
+        if(TRNno) updateUser.TRNno = TRNno
+        const user = await userModel.findByIdAndUpdate(id, updateUser, {new: true, runValidators: true})
+
+        return res.status(200).json({message: "user updated", user})
+    } catch (error) {
+        return next(new ErrorHandler(error.message,400))
+    }
 }))
 
 
