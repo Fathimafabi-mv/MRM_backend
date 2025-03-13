6f001d97b8b997484f5d98909a0779dd81f114c1
fathima fabi <fathimafabimv@gmail.com>
2025-03-13 09:37:57 +0530

    username aggregation

--- controller/orderController.js
+++ controller/orderController.js
@@ -3,6 +3,7 @@ const catchAsyncError = require("../Middleware/catchAsyncError");
 const ErrorHandler = require("../Utils/ErrorHandler");
 const orderModel=require("../Models/orderModel")
 const router=express.Router();
+const mongoose=require("mongoose")
 
 router.post("/add-order",catchAsyncError(async(req,res,next)=>{
     try {
@@ -24,9 +25,35 @@ router.post("/add-order",catchAsyncError(async(req,res,next)=>{
 
 router.get("/get-order",catchAsyncError(async(req,res,next)=>{
     try {
-        const getOrder=await orderModel.find()
-        res.status(200).json({getOrder})
-       
+        const getOrder = await orderModel.aggregate([
+            {
+                $lookup: {
+                    from: "users", // The users collection
+                    localField: "userId",
+                    foreignField: "_id",
+                    as: "userDetails"
+                }
+            },
+            {
+                $unwind: "$userDetails" // Convert array into an object
+            },
+            {
+                $project: {
+                    _id: 1,
+                    userId: 1,
+                    "userDetails.username": 1, // Include username from users collection
+                    products: 1,
+                    TotalAmount: 1,
+                    address: 1,
+                    OrderDate: 1,
+                    mobileNumber: 1,
+                    status: 1
+                }
+            }
+        ]);
+
+        res.status(200).json({ getOrder });
+
         
     } catch (error) {
         return next(new ErrorHandler(error.message,404))
@@ -43,12 +70,50 @@ router.delete("/delete-order/:id",catchAsyncError(async(req,res,next)=>{
 }))
 router.get("/get-order/:id",catchAsyncError(async(req,res,next)=>{
     try {
-        const {id}=req.params;
-        const getOrderID=await orderModel.find({_id:id})
-        if(!getOrderID){
-            return res.status(404).json({message:"Order not found!"})
+        // const {id}=req.params;
+        // const getOrderID=await orderModel.find({_id:id})
+        // if(!getOrderID){
+        //     return res.status(404).json({message:"Order not found!"})
+        // }
+        // res.status(200).json({getOrderID})
+
+        const { id } = req.params;
+        const getOrderID = await orderModel.aggregate([
+            {
+                $match: { _id: new mongoose.Types.ObjectId(id) } // Filter order by ID
+            },
+            {
+                $lookup: {
+                    from: "users", // Name of the users collection
+                    localField: "userId",
+                    foreignField: "_id",
+                    as: "userDetails"
+                }
+            },
+            {
+                $unwind: "$userDetails" // Convert array into an object
+            },
+            {
+                $project: {
+                    _id: 1,
+                    userId: 1,
+                    "userDetails.username": 1, // Fetch username from users collection
+                    products: 1,
+                    TotalAmount: 1,
+                    address: 1,
+                    OrderDate: 1,
+                    mobileNumber: 1,
+                    status: 1
+                }
+            }
+        ]);
+
+        if (!getOrderID || getOrderID.length === 0) {
+            return res.status(404).json({ message: "Order not found!" });
         }
-        res.status(200).json({getOrderID})
+
+        res.status(200).json({ getOrderID });
+
     } catch (error) {
         return next(new ErrorHandler(error.message,404))
     }
