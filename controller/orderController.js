67c5dd333d9999ced200ac47cfdeb0101428aa03
fathima fabi <fathimafabimv@gmail.com>
2025-03-18 10:19:12 +0530

    invoice created

--- controller/orderController.js
+++ controller/orderController.js
@@ -23,25 +23,23 @@ router.post("/add-order",catchAsyncError(async(req,res,next)=>{
     }
 }))
 
-router.get("/get-order",catchAsyncError(async(req,res,next)=>{
+router.get("/get-order", catchAsyncError(async (req, res, next) => {
     try {
         const getOrder = await orderModel.aggregate([
             {
                 $lookup: {
-                    from: "users", // The users collection
+                    from: "users",
                     localField: "userId",
                     foreignField: "_id",
                     as: "userDetails"
                 }
             },
-            {
-                $unwind: "$userDetails" // Convert array into an object
-            },
-            {
-                $project: {
+            { $unwind: { path: "$userDetails", preserveNullAndEmptyArrays: true } },
+            { 
+                $project: { 
                     _id: 1,
                     userId: 1,
-                    "userDetails.username": 1, // Include username from users collection
+                    "userDetails.username":1,
                     products: 1,
                     TotalAmount: 1,
                     address: 1,
@@ -54,11 +52,12 @@ router.get("/get-order",catchAsyncError(async(req,res,next)=>{
 
         res.status(200).json({ getOrder });
 
-        
     } catch (error) {
-        return next(new ErrorHandler(error.message,404))
+        return next(new ErrorHandler(error.message, 404));
     }
-}))
+}));
+
+
 router.delete("/delete-order/:id",catchAsyncError(async(req,res,next)=>{
     try {
         const{id}=req.params;
@@ -68,56 +67,57 @@ router.delete("/delete-order/:id",catchAsyncError(async(req,res,next)=>{
         return next(new ErrorHandler(error.message,404))
     }
 }))
-router.get("/get-order/:id",catchAsyncError(async(req,res,next)=>{
-    try {
-        // const {id}=req.params;
-        // const getOrderID=await orderModel.find({_id:id})
-        // if(!getOrderID){
-        //     return res.status(404).json({message:"Order not found!"})
-        // }
-        // res.status(200).json({getOrderID})
+router.get(
+    "/get-order/:id",
+    catchAsyncError(async (req, res, next) => {
+        try {
+            const { id } = req.params;
 
-        const { id } = req.params;
-        const getOrderID = await orderModel.aggregate([
-            {
-                $match: { _id: new mongoose.Types.ObjectId(id) } // Filter order by ID
-            },
-            {
-                $lookup: {
-                    from: "users", // Name of the users collection
-                    localField: "userId",
-                    foreignField: "_id",
-                    as: "userDetails"
-                }
-            },
-            {
-                $unwind: "$userDetails" // Convert array into an object
-            },
-            {
-                $project: {
-                    _id: 1,
-                    userId: 1,
-                    "userDetails.username": 1, // Fetch username from users collection
-                    products: 1,
-                    TotalAmount: 1,
-                    address: 1,
-                    OrderDate: 1,
-                    mobileNumber: 1,
-                    status: 1
-                }
+            // Validate if ID is a valid ObjectId
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                return res.status(400).json({ message: "Invalid Order ID" });
             }
-        ]);
 
-        if (!getOrderID || getOrderID.length === 0) {
-            return res.status(404).json({ message: "Order not found!" });
+            const getOrderID = await orderModel.aggregate([
+                {
+                    $match: { _id: new mongoose.Types.ObjectId(id) },
+                },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "userId",
+                        foreignField: "_id",
+                        as: "userDetails",
+                    },
+                },
+                { $unwind: { path: "$userDetails", preserveNullAndEmptyArrays: true } },
+                {
+                    $project: {
+                        _id: 1,
+                        userId: 1,
+                        "userDetails.username": 1,
+                        "userDetails.TRNno":1,
+                        products: 1,
+                        TotalAmount: 1,
+                        address: 1,
+                        OrderDate: 1,
+                        mobileNumber: 1,
+                        status: 1,
+                    },
+                },
+            ]);
+
+            if (!getOrderID || getOrderID.length === 0) {
+                return res.status(404).json({ message: "Order not found!" });
+            }
+
+            res.status(200).json({ getOrderID: getOrderID[0] || {} });
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
         }
+    })
+);
 
-        res.status(200).json({ getOrderID });
-
-    } catch (error) {
-        return next(new ErrorHandler(error.message,404))
-    }
-}))
 router.patch("/update-order/:id",catchAsyncError(async(req,res,next)=>{
     try {
         const{id}=req.params;
